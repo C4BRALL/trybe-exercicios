91271afc42b7d2902a1ad574255a7ba5c330c7d9
C4BRALL <jcabral304@gmail.com>
2021-11-26 16:01:36 -0300

    :up: Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas

--- Fundamentos_do_Desenvolvimento_Web/Blocos/bloco-4-introduçao-a-javascript-e-logica-de-programaçao/javaScript-primeiros-passos/scripts.js
+++ Fundamentos_do_Desenvolvimento_Web/Blocos/bloco-4-introduçao-a-javascript-e-logica-de-programaçao/javaScript-primeiros-passos/scripts.js
@@ -5,3 +5,26 @@ let birthYear = "1999";
 console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+patientId = "50";
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+
+const base = 5;
+const height = 8;
+const area = base * height;
+const perimeter = 2*base+2*height;
+
+console.log(base);
+console.log(height);
+console.log(area);
+console.log(perimeter);
