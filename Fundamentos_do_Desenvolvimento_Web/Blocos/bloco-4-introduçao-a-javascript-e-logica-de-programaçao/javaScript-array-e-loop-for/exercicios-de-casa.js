be2a2c8d0ed777e055e050716e17c99b7521ef1c
C4BRALL <jcabral304@gmail.com>
2021-11-29 18:13:25 -0300

    🗸 exercicio 2

--- Fundamentos_do_Desenvolvimento_Web/Blocos/bloco-4-introduçao-a-javascript-e-logica-de-programaçao/javaScript-array-e-loop-for/exercicios-de-casa.js
+++ Fundamentos_do_Desenvolvimento_Web/Blocos/bloco-4-introduçao-a-javascript-e-logica-de-programaçao/javaScript-array-e-loop-for/exercicios-de-casa.js
@@ -1,7 +1,14 @@
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for(let number of numbers){
     console.log(number);
 }
 
-/////////////////////////////////////////////////////////////////////////////////
+//  resoluçao achada https://www.delftstack.com/pt/howto/javascript/javascript-sum-of-array/
+
+let soma = 0;
+for(let index = 0; index < numbers.length; index += 1){
+    soma += numbers[index];
+}
+console.log("soma da array: " + soma);
