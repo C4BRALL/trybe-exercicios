3ca322dc06ddca1aeeb6d42d673fe4f3185bb706
C4BRALL <jcabral304@gmail.com>
2021-11-29 14:18:42 -0300

    :package: Taxa Metabólica Basal

--- Academia de lógica/29-november/taxa-metabolica-basal.js
+++ Academia de lógica/29-november/taxa-metabolica-basal.js
@@ -15,4 +15,15 @@ O retorno deve ser no seguinte formato:
 "A taxa metabólica basal é: x Kcal"
 ---------------------------------------
 */
+let age = 23;
+let sex = "F";
+let weight = 88;
+let height = 1.75;
 
+if(sex.toLowerCase() === "m"){
+    console.log((height*6.25)+(weight*9.99)-(age*4.92)+5);
+}else if(sex.toLowerCase()  === "f"){
+    console.log((height*6.25)+(weight*9.99)-(age*4.92)-161);
+}else{
+    console.log("error")
+}
