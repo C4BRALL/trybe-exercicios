9a1d6e6c94ea93f4d165cd1d44adea9a6b01f173
C4BRALL <jcabral304@gmail.com>
2021-12-06 15:28:48 -0300

    :package: trabalhando com elementos

---
+++ Fundamentos_do_Desenvolvimento_Web/Blocos/bloco-5-Dom-Eventos-e-WebStorage/JavaScript-Trabalhando-com-elementos/script.js
@@ -0,0 +1,13 @@
+console.log(document.getElementById('primeiroFilhoDoFilho').parentElement);
+
+console.log(document.querySelector('#elementoOndeVoceEsta').parentElement);
+
+console.log(document.querySelector('#pai').firstElementChild);
+
+console.log(document.querySelector('#elementoOndeVoceEsta').previousElementSibling);
+
+console.log(document.querySelector('#elementoOndeVoceEsta').parentElement.nextSibling);
+
+console.log(document.getElementById('elementoOndeVoceEsta').parentElement.lastElementChild.previousElementSibling);
+
+console.log(document.getElementById('pai').lastElementChild.previousElementSibling);
