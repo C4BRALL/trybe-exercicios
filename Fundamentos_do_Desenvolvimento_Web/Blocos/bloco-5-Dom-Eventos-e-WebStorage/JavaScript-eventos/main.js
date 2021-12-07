d2089e1c0ab9d5ba4cfe36ac46be1c005976b50b
C4BRALL <jcabral304@gmail.com>
2021-12-07 19:14:56 -0300

    :up: New eventListener input function(event) added succesfully

--- Fundamentos_do_Desenvolvimento_Web/Blocos/bloco-5-Dom-Eventos-e-WebStorage/JavaScript-eventos/main.js
+++ Fundamentos_do_Desenvolvimento_Web/Blocos/bloco-5-Dom-Eventos-e-WebStorage/JavaScript-eventos/main.js
@@ -4,23 +4,24 @@ const thirdLi = document.getElementById('third-li');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('my-spotrybefy');
 
-
+const newTech = document.querySelector('.tech');
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-firstLi.addEventListener('click', classTech);
-function classTech(tech) {
-    firstLi.className = tech;
-    console.log(tech.target);
-    console.log(tech.type);
-    return firstLi;
+function classTech(event) {
+    newTech.classList.remove('tech');
+    newTech.classList.add('tech');
+    input.value = '';
 }
+firstLi.addEventListener('click', classTech);
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
-
+input.addEventListener('input', function(event) {
+    newTech.innerText = event.target.value;
+})
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
