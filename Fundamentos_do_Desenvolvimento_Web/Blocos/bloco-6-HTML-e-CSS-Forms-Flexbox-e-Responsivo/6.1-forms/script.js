8f339e77bc0e5bf8e0417005b31b0ef2d2e62b81
Cabral <jcabral304@gmail.com>
2022-01-04 19:32:21 -0300

    6.1

---
+++ Fundamentos_do_Desenvolvimento_Web/Blocos/bloco-6-HTML-e-CSS-Forms-Flexbox-e-Responsivo/6.1-forms/script.js
@@ -0,0 +1,9 @@
+const stateContainer = document.getElementById('state');
+function createOption (stateAbre, state) {
+    let stateArray = []
+    for (let i; i < stateArray.length; i+=1) {
+        const optionE = document.createElement('option');
+        optionE.value = stateAbre;
+    }
+
+}
