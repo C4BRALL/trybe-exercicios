e1606e99c203f62baf25048688927c7c3188e256
Cabral <jcabral304@gmail.com>
2022-01-05 12:43:50 -0300

    1 parte feita

--- Fundamentos_do_Desenvolvimento_Web/Blocos/bloco-6-HTML-e-CSS-Forms-Flexbox-e-Responsivo/6.1-forms/script.js
+++ Fundamentos_do_Desenvolvimento_Web/Blocos/bloco-6-HTML-e-CSS-Forms-Flexbox-e-Responsivo/6.1-forms/script.js
@@ -1,9 +1,13 @@
-const stateContainer = document.getElementById('state');
-function createOption (stateAbre, state) {
-    let stateArray = []
-    for (let i; i < stateArray.length; i+=1) {
-        const optionE = document.createElement('option');
-        optionE.value = stateAbre;
+const stateContainer = document.querySelector('#state');
+let states = ['Ceara', 'Rio de Janeiro', 'Sao Paulo'];
+function createOption () {
+    for (i of states) {
+        const newOption = document.createElement('option');
+        newOption.innerText = [i];
+        newOption.value = [i];
+        stateContainer.appendChild(newOption);
     }
-
 }
+createOption();
+
+// Criar uma function para pegar a abreviaçao do nome dos estados e por como value nesta function.
