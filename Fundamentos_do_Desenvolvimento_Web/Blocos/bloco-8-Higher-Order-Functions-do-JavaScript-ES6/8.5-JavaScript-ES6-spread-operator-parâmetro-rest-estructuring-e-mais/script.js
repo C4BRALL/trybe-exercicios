79784bfe4177f7b9668c881d30ec429a67ef9c83
Cabral <jcabral304@gmail.com>
2022-01-24 19:44:06 -0300

    exercise 4

--- Fundamentos_do_Desenvolvimento_Web/Blocos/bloco-8-Higher-Order-Functions-do-JavaScript-ES6/8.5-JavaScript-ES6-spread-operator-parâmetro-rest-estructuring-e-mais/script.js
+++ Fundamentos_do_Desenvolvimento_Web/Blocos/bloco-8-Higher-Order-Functions-do-JavaScript-ES6/8.5-JavaScript-ES6-spread-operator-parâmetro-rest-estructuring-e-mais/script.js
@@ -42,3 +42,38 @@ const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and l
 console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
 console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures,'
 console.log("------------------------------------------------------------------");
+
+// 4 - Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+// escreva filterPeople abaixo
+const filterPeople = people.filter((people) => people.bornIn >= 1901 && people.bornIn <= 1999 && people.nationality === 'Australian');
+console.log(filterPeople);
+console.log("------------------------------------------------------------------");
