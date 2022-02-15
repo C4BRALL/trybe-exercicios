b472c9a9c14b0d22e970668ffd6ddea9e6781af4
Cabral <jcabral304@gmail.com>
2022-02-15 13:44:06 -0300

    ReactClass

--- Desenvolvimento-Front-end/bloco-10-Introdução-a-React/hello-world-no-react/teste.js
+++
@@ -1,6 +0,0 @@
-function helloWorld (nome, sobrenome) {
-  return <h1>Hello, {`${nome} ${sobrenome}`}</h1>;
-}
-
-const element = helloWorld("Jorge", "Maravilha");
-const container = <div>{element}</div>;
