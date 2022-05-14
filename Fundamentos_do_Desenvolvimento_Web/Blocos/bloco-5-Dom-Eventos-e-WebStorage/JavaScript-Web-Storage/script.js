bfd6769480d6852a9c2f4811f7dfc5535cff5a2d
Cabral <jcabral304@gmail.com>
2022-05-14 11:31:08 -0300

    Dockerized

--- Fundamentos_do_Desenvolvimento_Web/Blocos/bloco-5-Dom-Eventos-e-WebStorage/JavaScript-Web-Storage/script.js
+++
@@ -1,9 +0,0 @@
-let backgroundColor = document.querySelector('#bColor');
-let fontColor = document.querySelector('#fColor');
-
-backgroundColor.onchange = function() {
-    document.body.style.backgroundColor = this.value;
-}
-fontColor.onchange = function() {
-    document.body.style.color = this.value;
-}
